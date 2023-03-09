9181cdb8589ca4a9c8bc3c20ed2876ec9519759b
QNHFIwWGdOq1kYGaQM4A <120741042+QNHFIwWGdOq1kYGaQM4A@users.noreply.github.com>
2023-03-10 02:12:23 +0800

    if you found this, :(

---
+++ rAeffVJZYFPD14o5SKuZ.lua
@@ -0,0 +1,13 @@
+Status = "Enabled"
+Emojis = {
+["Earth"] = "🌍";
+["Rocket"] = "🚀";
+["Sun"] = "☀";
+["Neptune"] = "🔵";
+["Moon"] = "🌑";
+}
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Mightix",
+	Text = "Loaded",
+})
